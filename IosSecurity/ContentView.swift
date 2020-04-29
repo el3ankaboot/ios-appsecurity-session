be657a64c5b12f8d52e9707507e872b329cf93a4
el3ankaboot <gamal.m.esmail@gmail.com>
2020-04-29 12:26:07 +0200

    Initial Commit

---
+++ IosSecurity/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IosSecurity
+//
+//  Created by Gamal Mostafa on 4/29/20.
+//  Copyright © 2020 Gamal Mostafa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
